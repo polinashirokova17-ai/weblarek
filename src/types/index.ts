5f900dbcfaafeb55042ef0687bd684ef2653f4b2
Poli Key <polikey.00@mail.ru>
2026-05-15 10:53:03 +0300

    доработка

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,52 +1,56 @@
 // src/types/index.ts
 
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+export type ApiPostMethods = "POST" | "PUT" | "DELETE";
 
 export interface IApi {
-    get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+  get<T extends object>(uri: string): Promise<T>;
+  post<T extends object>(
+    uri: string,
+    data: object,
+    method?: ApiPostMethods,
+  ): Promise<T>;
 }
 
 // ===== ТИПЫ ДЛЯ МОДЕЛЕЙ =====
 
-export type TPayment = 'online' | 'cash';
+export type TPayment = 'card' | 'cash';
 
 export interface IProduct {
-    id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number | null;
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
 }
 
 export interface IBuyer {
-    payment: TPayment;
-    email: string;
-    phone: string;
-    address: string;
+  payment: TPayment | null;
+  email: string;
+  phone: string;
+  address: string;
 }
 
 export interface IFormErrors {
-    payment?: string;
-    address?: string;
-    email?: string;
-    phone?: string;
+  payment?: string;
+  address?: string;
+  email?: string;
+  phone?: string;
 }
 
 // ===== ТИПЫ ДЛЯ API =====
 
 export interface IProductsResponse {
-    items: IProduct[];
-    total: number;
+  items: IProduct[];
+  total: number;
 }
 
 export interface IOrder extends IBuyer {
-    items: string[];
-    total: number;
+  items: string[];
+  total: number;
 }
 
 export interface IOrderResult {
-    id: string;
-    total: number;
+  id: string;
+  total: number;
 }
